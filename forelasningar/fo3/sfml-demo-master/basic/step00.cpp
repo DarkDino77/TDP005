36c647a41e9a532e6e944e4d5ceb09bb1174afcb
Henrik Hårshagen <henha806@student.liu.se>
2023-12-04 09:48:32 +0100

    Added spitter sprites

--- forelasningar/fo3/sfml-demo-master/basic/step00.cpp
+++ forelasningar/fo3/sfml-demo-master/basic/step00.cpp
@@ -1,4 +1,4 @@
-#include <SFML/Graphics.hpp>
+ #include <SFML/Graphics.hpp>
 
 /**
  * Steg 0: Skapa ett fönster
@@ -11,10 +11,29 @@
 const size_t width = 1024;
 const size_t height = 768;
 
-int main() {
-    sf::RenderWindow window{sf::VideoMode{width, height}, "Demo"};
-
-    sf::sleep(sf::milliseconds(3000));
-
-    return 0;
-}
+int main()
+ {
+     // Create the main window
+     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+ 
+     // Start the game loop
+     while (window.isOpen())
+     {
+         // Process events
+         sf::Event event;
+         while (window.pollEvent(event))
+         {
+             // Close window : exit
+             if (event.type == sf::Event::Closed)
+                 window.close();
+         }
+ 
+         // Clear screen
+         window.clear();
+ 
+         // Update the window
+         window.display();
+     }
+ 
+     return EXIT_SUCCESS;
+ }
